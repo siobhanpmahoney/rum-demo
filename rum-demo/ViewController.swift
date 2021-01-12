89cdc1c31c46c2d211c6c4853a9e3d3c005f3bd0
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-01-11 23:34:36 -0500

    Initial Commit

---
+++ rum-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rum-demo
+//
+//  Created by Adam Kaczmarek on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
