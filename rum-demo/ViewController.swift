3fc39c24ee86206c7bad0f788d3da1054188dda7
Adam Kaczmarek <adam.kaczmarek@datadoghq.com>
2021-01-11 23:49:34 -0500

    sample app

--- rum-demo/ViewController.swift
+++ rum-demo/ViewController.swift
@@ -13,7 +13,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var count: Int = 0
+    
+    @IBAction func didTapButton() {
+        present(BlueViewController(), animated: true)
+    }
+    
+    @IBAction func didIncrementButton() {
+        count += 1
+        counterOutput.text = String(count)
+    }
+    
+    @IBOutlet weak var counterOutput: UILabel!
+    
+    @IBAction func didErrorButton() {
+        print("Something went wrong....")
+    }
 
 }
 
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+}
